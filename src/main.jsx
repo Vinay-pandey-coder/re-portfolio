27b67291a379efffa9ae5619f5543c7cce6ba5f5
vinay pandey <abhi837688@gmail.com>
2025-06-28 16:28:45 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'lenis/dist/lenis.css'
 import App from './App.jsx'
-import 'boxicons';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
